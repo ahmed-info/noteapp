0af665f36bc79183f488b867c3873b03dc99ae2e
unknown <ahmedscript11@gmail.com>
2022-04-19 03:06:55 +0300

    second  commit

--- functions.php
+++ functions.php
@@ -2,4 +2,39 @@
 function filterRequest($requestName){
    return htmlspecialchars(strip_tags($_POST[$requestName]));
 }
+//1 MB = 1024*1024 = 1048576 
+// Define => const
+define('MB',1048576);
+
+//name of file requestd
+function imageUpload($imageRequest)
+{
+   global $msgError;
+   $imagename = $_FILES[$imageRequest]['name'];
+   $imagetmp = $_FILES[$imageRequest]['tmp_name'];
+   $imagesize = $_FILES[$imageRequest]['size'];
+   $allowExt = array("jpg","png","gif","mp3","pdf");
+   $strToArray = explode(".", $imagename);
+   $ext = end($strToArray);
+   $ext = strtolower($ext);
+   if(!empty($imagename) && !in_array($ext,$allowExt)){
+      $msgError[] = "EXT is Error";
+   }
+   if($imagesize > 10 *MB){
+      $msgError[] = "Size is Error";
+   }
+
+   if(empty($msgError)){
+   move_uploaded_file($imagetmp, "../upload/".$imagename);
+   return $imagename;
+   }else{
+      return "fail";
+   }
+}
+
+function deleteFile($dir, $imagename){
+   if(file_exists($dir."/".$imagename)){
+      unlink($dir."/".$imagename);
+   }
+}
 ?>
--- notes/delete.php
+++ notes/delete.php
@@ -2,7 +2,7 @@
  include "../connect.php";
  
  $noteid = filterRequest('id');
-
+$imagename = filterRequest('imagename');
 
 
  $stmt = $con->prepare("DELETE FROM notes WHERE notes_id =?");
@@ -11,6 +11,7 @@
 
  $count = $stmt->rowCount();
  if($count > 0){
+    deleteFile("../upload",$imagename);
     echo json_encode(array("status"=>"success"));
  }else{
     echo json_encode(array("status"=>"error"));
